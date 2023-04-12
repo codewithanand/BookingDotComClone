9dc7299fd31354c13e173146c6cb24c07e99a949
codebyaanand@gmail.com <codebyaanand@gmail.com>
2023-04-12 14:12:47 +0530

    navbar update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './navbar.scss'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="container">
-            <div className="logo">BookingApp</div>
+            <div className="logo"><Link to="/" >BookingApp</Link></div>
             <div className="buttons">
                 <button>Register</button>
                 <button>Sign in</button>
