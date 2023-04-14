f94cb45a51084598cceca6a8e72656d33b05d588
codebyaanand@gmail.com <codebyaanand@gmail.com>
2023-04-14 11:31:36 +0530

    authentication

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from 'bcryptjs'
 import {createError} from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res, next) => {
     try{
@@ -26,7 +27,11 @@ export const login = async (req, res, next) => {
 
         const checkPassword = await bcrypt.compare(req.body.password, user.password)
         if(!checkPassword) return next(createError(400, "Wrong password or username!"))
-        res.status(200).json("Logged in successfully!")
+
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT)
+
+        const {password, isAdmin, ...otherDetails} = user._doc
+        res.cookie("access_token", token, {httpOnly: true,}).status(200).json({...otherDetails})
     }
     catch(err){
         next(err)
